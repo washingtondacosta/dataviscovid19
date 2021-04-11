805785a3e54e5149c943648feb0177571f5b9b21
Washington da Costa Silva <washington3636@gmail.com>
2021-04-11 15:57:43 -0300

    backtotop

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,3 +12,18 @@ $(function() {
     }
   });
 });
+
+var btn = $('#buttonTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
